9adf7231ab11d2ba8acecbc28a93342feffcddeb
SeliniumNov2025 <thiruppathinarasimman35@gmail.com>
2025-11-08 21:33:59 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+	
+	
+	public static void main(String[] args) {
+		float version=91.0f;
+		String developerCompanyName="Google";
+		boolean isBetaType=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println("Chrome Version is "+ version);
+		System.out.println("Chrome Developer Company Name is "+developerCompanyName);
+		System.out.println("Is Chrome Beta type- "+isBetaType);
+		System.out.println("Chrome Release Year is "+releaseYear);
+		System.out.println("Chrome ShortKey is "+shortcutKey);
+	}
+	
+	
+
+}
