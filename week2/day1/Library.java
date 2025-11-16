ebf47a70eeb2e7edbd7ee0bbe69988ff8377f55f
SeliniumNov2025 <thiruppathinarasimman35@gmail.com>
2025-11-16 14:06:53 +0530

    Add files via upload

---
+++ week2/day1/Library.java
@@ -0,0 +1,23 @@
+package week2.day1;
+
+public class Library {
+	
+	public String addBook(String bookTitle ) {
+		System.out.println(bookTitle + " Book added successfully");
+		return bookTitle;
+		
+	} 
+	public void issueBook() {
+		System.out.println("Book issued successfully");
+	}
+	
+	public static void main(String[] args) {
+		
+		Library library = new Library();
+		library.addBook("Harry Potter");
+		library.issueBook();
+		
+		
+	}
+
+}
---
+++ week2/day1/LibraryManagement.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class LibraryManagement {
+	
+	public static void main(String[] args) {
+		
+		Library library = new Library();
+		
+		library.addBook("Harry Potter");
+		library.issueBook();
+		
+	}
+
+}
